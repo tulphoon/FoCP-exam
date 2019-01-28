873e7e25ffff1db0f935029dcb2660f18212c520
Dawid Osuchowski <dwdosuchowski@gmail.com>
2019-01-29 00:31:45 +0100

    Numbers: Finish read

---
+++ numbers/read.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <fstream>
+
+#define COLUMN_NUMBER 5
+
+void read(const std::string & fileName, double arr[]) {
+    std::ifstream is(fileName);
+    if(is) { 
+        int counter = 0;
+        while(!is.eof()) {
+            int number;
+            for(int i = 0; i < COLUMN_NUMBER; i++) {
+                is >> number;
+                arr[i] += number;
+            }
+            counter++;
+        }
+        for(int i = 0; i < COLUMN_NUMBER; i++) {
+            arr[i] = arr[i] / counter;
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    double array[COLUMN_NUMBER] = {};
+    read("read.txt", array);
+    return 0;
+}
