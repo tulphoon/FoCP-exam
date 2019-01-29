4eaa6a87b98b36f5466d8d3db713b1eebd4c0891
Dawid Osuchowski <dwdosuchowski@gmail.com>
2019-01-29 15:23:37 +0100

    Numbers: finish sort function

--- numbers/main.cpp
+++ numbers/main.cpp
@@ -3,6 +3,20 @@
 
 #define COLUMN_NUMBER 5
 
+// sort using bubble sort
+void sort(double arr[]) {
+    int tmp;
+    for(int i = 0; i < COLUMN_NUMBER - 1; i++) {
+        for(int j = 0; j < COLUMN_NUMBER - i - 1; j++) {
+            if(arr[j] > arr[j+1]) {
+                tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;    
+            }
+        }
+    }
+}
+
 // Funkcja read
 void read(const std::string & fileName, double arr[]) {
     std::ifstream is(fileName);
@@ -26,5 +40,6 @@ int main(int argc, char const *argv[])
 {
     double array[COLUMN_NUMBER] = {};
     read("read.txt", array);
+    sort(array);
     return 0;
 }
