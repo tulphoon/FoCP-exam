52f31a5f4687938035f87662ba467cf68967bf11
Dawid Osuchowski <dwdosuchowski@gmail.com>
2019-01-29 19:50:42 +0100

    Pictures: add read function

--- pictures/main.cpp
+++ pictures/main.cpp
@@ -1,5 +1,30 @@
+#include <string>
+#include <fstream>
+
+#define W 10
+#define K 10
+
+void read(const std::string & fileName, int picture[W][K], int & rows, int & columns) {
+    std::ifstream inStream(fileName);
+    if(inStream) {
+        int pixel;
+        inStream >> rows >> columns;
+        if(rows > W or columns > K) return;
+        
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < columns; j++) {
+                inStream >> pixel;
+                picture[i][j] = pixel;
+            }
+        }
+        inStream.close();
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int rows, columns;
+    int array[W][K];
+    read("picture.txt", array, rows, columns);
     return 0;
 }
