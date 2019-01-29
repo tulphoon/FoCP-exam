b6770b3bc57466161ff02014007e51a181f71853
Dawid Osuchowski <dwdosuchowski@gmail.com>
2019-01-29 20:13:22 +0100

    Pictures: created write function

--- pictures/main.cpp
+++ pictures/main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <fstream>
+#include <iomanip>
 
 #define W 10
 #define K 10
@@ -10,6 +11,7 @@ void read(const std::string & fileName, int picture[W][K], int & rows, int & col
         int pixel;
         inStream >> rows >> columns;
         if(rows > W or columns > K) return;
+        else if(rows < 0 or columns < 0) return;
         
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < columns; j++) {
@@ -21,10 +23,26 @@ void read(const std::string & fileName, int picture[W][K], int & rows, int & col
     }
 }
 
+void write(const std::string & fileName, int picture[W][K], const int & rows, const int & columns) {
+    std::ofstream outStream(fileName);
+    if(outStream) {
+        outStream << std::setw(5) << rows << std::setw(5) << columns << std::endl;
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < columns; j++) {
+                outStream << std::setw(5) << picture[i][j];
+            }
+            outStream << std::endl;
+        }
+        outStream.close();
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-    int rows, columns;
+    int rows = -1;
+    int columns = -1;
     int array[W][K];
     read("picture.txt", array, rows, columns);
+    write("outputPicture.txt", array, rows, columns);
     return 0;
 }
